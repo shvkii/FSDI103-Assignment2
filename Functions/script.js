7d2672be9cabf6fbee2824e75481db880b57b710
shvkii <92766830+shvkii@users.noreply.github.com>
2021-11-12 18:38:11 -0800

    Add files via upload

---
+++ Functions/script.js
@@ -0,0 +1,19 @@
+//Define the function
+
+function sayHello(userName='User'){
+    console.log("Hello "+userName+" !");
+}
+
+function max(number1, number2){
+    if(number1>number2){
+        console.log(number1);
+    }
+    else{
+        console.log(number2);
+    }
+
+}
+
+max(5,9)
+max(13,12)
+max(16,20)
---
+++ Functions/script2.js
@@ -0,0 +1,19 @@
+let x=Number(prompt("Enter number 1:"));
+let y=Number(prompt("Enter number 2:"));
+
+function sum(number1, number2){
+    document.write(`<p>${number1} + ${number2} = ${number1+number2}</p>`);
+}
+function sub(number1, number2){
+    document.write(`<p>${number2} - ${number1} = ${number2-number1}</p>`);
+}
+function mult(number1, number2){
+    document.write(`<p>${number2} * ${number1} = ${number2*number1}</p>`);
+}
+function div(number1, number2){
+    document.write(`${number2} / ${number1} = ${number2/number1}`);
+}
+sum(x,y);
+sub(x,y);
+mult(x,y);
+div(x,y);
